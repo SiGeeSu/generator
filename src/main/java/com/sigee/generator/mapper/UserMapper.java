9087546403cfebd2214db4a17c45c13f97041d5b
SiGeeSu <526860193@qq.com>
2018-11-06 21:01:32 +0800

    Initial commit

---
+++ src/main/java/com/sigee/generator/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.sigee.generator.mapper;
+
+import com.sigee.generator.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
